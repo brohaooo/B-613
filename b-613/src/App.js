99ad553cb02847cfd77ccaa278d3b58a7f2d9969
HiDoBo <62140338+HiDoBo@users.noreply.github.com>
2022-04-15 17:25:54 +0800

    Add files via upload

--- b-613/src/App.js
+++ b-613/src/App.js
@@ -3,6 +3,11 @@ import 'antd/dist/antd.css';
 import LoginPage from "./pages/LoginPage.js";
 import RegisterPage from "./pages/RegisterPage";
 import HomePage from "./pages/HomePage.js";
+import SucceedToRegisterPage from "./pages/SucceedToRegisterPage.js";
+import InfoEdit from "./pages/InfoEdit";
+import Profile from "./pages/Profile";
+
+
 
 function App() {
   return (
@@ -11,6 +16,10 @@ function App() {
         <Route path="/" element={<LoginPage />} exact></Route>
         <Route path="/register" element={<RegisterPage />} exact></Route>
         <Route path="/home" element={<HomePage />} exact></Route>
+        <Route path="/success" element={<SucceedToRegisterPage />} exact></Route>  
+        <Route path="/edit" element={<InfoEdit />} exact></Route>  
+        <Route path="/profile" element={<Profile />} exact></Route>  
+
       </Routes>
     </BrowserRouter>
       
