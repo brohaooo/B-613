462438c3403524f57d14b39804f7f49710058542
HiDoBo <62140338+HiDoBo@users.noreply.github.com>
2022-04-18 03:00:08 +0800

    Add files via upload

--- b-613/src/pages/AdministratorPage.js
+++ b-613/src/pages/AdministratorPage.js
@@ -4,17 +4,29 @@ import { RightCircleOutlined,BellOutlined, EyeTwoTone,TeamOutlined, UserOutlined
 import 'antd/dist/antd.css';
 import { useState } from 'react';
 import axios from 'axios';
-import cookie from 'react-cookies'
+import cookie from 'react-cookies';
 
 function Admin () {
   var allCard = [];
   var res;
   var that = this;
   var id;
-  const goToModify = (id)=>{
-    cookie.save('id',id);
-    window.location.href="/modify";
-   }
+
+  const submit = (id) => {
+    
+    axios.put('http://localhost:8080/api/modifyPassword/' + id, {
+      password: password,
+    })
+    .then(function (response) {
+      window.location.href="/admin";
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  }; 
+
+  
   const show = () => {
     axios.get('http://localhost:8080/api/users/', {
  })
@@ -33,6 +45,7 @@ function Admin () {
 
   }; 
   const [dataset,setDataset] = useState([]);
+  const [password,setPassword] = useState('');
 
 
     return (
@@ -60,14 +73,23 @@ function Admin () {
         Gender:{item.gender}<br></br>
         User Email:{item.userEmail}<br></br>
         User Password:{item.password}<br></br>
-        <Button 
-                
+        <Input type="text" class="form-control"  placeholder="New Password"
+                    onChange={(e) => {
+                    setPassword(e.target.value);
+                    console.log(password)
+                    }}/>
+
+          <Button type="primary" htmlType="submit" className="login-form-button"
+                onClick={ () => {submit(item.id)}}
             >
-                Double click to modify its password
+                Confirm
             </Button>
       </List.Item>
+
     )}
   />,
+
+
       </div>)
 
 };
