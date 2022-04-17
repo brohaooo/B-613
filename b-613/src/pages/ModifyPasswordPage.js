3082b74f606faa678a31ab224ad651cbd62d802a
HiDoBo <62140338+HiDoBo@users.noreply.github.com>
2022-04-17 21:54:35 +0800

    Add files via upload
    改密码真正页面 配合forgotPasswordPage食用

---
+++ b-613/src/pages/ModifyPasswordPage.js
@@ -0,0 +1,86 @@
+
+import './RegisterPage.css';
+import { Form, Input, Button, Checkbox } from 'antd';
+import { MailOutlined,UserOutlined, LockOutlined } from '@ant-design/icons';
+import 'antd/dist/antd.css';
+import { useState } from 'react';
+import axios from "axios";
+import cookie from 'react-cookies'
+
+
+const password = cookie.load('password');
+const id = cookie.load('id');
+
+
+
+
+function RegisterPage() {
+  const goToLogin = () => {
+    window.location.href="/";
+  };
+  const goToRegister = () => {
+    window.location.href="/register";
+  };
+  const goToSuccess = () => {
+    window.location.href="/success";
+  };
+ 
+  const submit = () => {
+    
+    axios.put('http://localhost:8080/api/modifyPassword/' + id, {
+      password: password,
+    })
+    .then(function (response) {
+      console.log(response);
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+    setPassword('');
+  }; 
+  
+  
+
+  const [password,setPassword] = useState('');
+
+  return (
+<div className='page'>
+    <div className="container">
+        <div class="panel-heading text-center">
+            <h1 class="panel-title">
+                <b>Welcome to modify your password.</b>
+            </h1>
+        </div>
+
+
+
+    
+        <div className="panel-body">
+      
+
+            <div>
+                <span class="input-group-addon">Your New Password:</span>
+                <Input type="text" class="form-control"  placeholder="New Password"
+                    onChange={(e) => {
+                    setPassword(e.target.value);
+                    console.log(password)
+                    }}/>
+            </div>
+
+            
+
+            <Button type="primary" htmlType="submit" className="login-form-button"
+                onClick={submit}
+            >
+                Confirm
+            </Button>
+        </div>
+    </div>
+</div>
+                
+
+               
+  );
+}
+
+export default RegisterPage;
